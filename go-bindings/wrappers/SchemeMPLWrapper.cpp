507c07dc0fd37684725d9f1ac6ba630cf540de48
williams824 <produqzai824@gmail.com>
2021-07-28 00:36:24 +0800

    update pop scheme sk unhardened

--- go-bindings/wrappers/SchemeMPLWrapper.cpp
+++ go-bindings/wrappers/SchemeMPLWrapper.cpp
@@ -624,17 +624,23 @@ HandleRetWrapper PopSchemeMPLDeriveChildSk(
     return ret;
 }
 
-PrivateKeyWrapper PopSchemeMPLDeriveChildSkUnhardened(
+HandleRetWrapper PopSchemeMPLDeriveChildSkUnhardened(
     PopSchemeMPLWrapper popScheme,
     PrivateKeyWrapper master,
     uint32_t index)
 {
-    bls::PopSchemeMPL *popSchemeMpl = (bls::PopSchemeMPL *)popScheme;
-    bls::PrivateKey *masterPrivateKey = (bls::PrivateKey *)master;
-    bls::PrivateKey childSk =
-        (*popSchemeMpl).DeriveChildSkUnhardened(*masterPrivateKey, index);
-    bls::PrivateKey *childPrivateKey = new bls::PrivateKey(childSk);
-    return (void *)(childPrivateKey);
+    HandleRetWrapper ret = {0};
+    try {
+        bls::PopSchemeMPL *popSchemeMpl = (bls::PopSchemeMPL *)popScheme;
+        bls::PrivateKey *masterPrivateKey = (bls::PrivateKey *)master;
+        bls::PrivateKey childSk =
+            (*popSchemeMpl).DeriveChildSkUnhardened(*masterPrivateKey, index);
+        bls::PrivateKey *childPrivateKey = new bls::PrivateKey(childSk);
+        ret.handle = (PrivateKeyWrapper)(childPrivateKey);
+    }catch (std::exception &e){
+        ret.err = strdup(e.what());
+    }
+    return ret;
 }
 
 BytesWrapper PopSchemeMPLDeriveChildPkUnhardened(
